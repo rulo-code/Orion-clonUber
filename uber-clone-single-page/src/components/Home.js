009134a9b8012acc1ab64ae8ec076ebd0cd87ae9
Andres <drew.lizcano@gmail.com>
2019-09-13 13:35:39 -0500

    creando animacion de inicio

--- uber-clone-single-page/src/components/Home.js
+++ uber-clone-single-page/src/components/Home.js
@@ -4,14 +4,26 @@ import './styles/Home.css'
 
 import barrainclinada from './styles/images/finales/Recurso7.png'
 import ciudad from './styles/images/finales/Recurso2.png'
+import nube from './styles/images/finales/Recurso8.png'
+import arbol1 from './styles/images/finales/Recurso4.png'
+import arbol2 from './styles/images/finales/Recurso5.png'
 
 function Home(){
   return(
     <div className="home">
-      Hola
-      <img className="barra" src={barrainclinada} alt="barra"/>
-      <img className="ciudad" src={ciudad} alt="ciudad"/>
+      <img className="home__barra" src={barrainclinada} alt="barra"/>
+      <img className="home__ciudad" src={ciudad} alt="ciudad"/>
+      <div className="home__nuves">
+        <img className="nuves__nuve uno" src={nube} alt="nube"/>
+        <img className="nuves__nuve dos" src={nube} alt="nube"/>
+        <img className="nuves__nuve tres" src={nube} alt="nube"/>
+      </div>
+      <div className="home__arboles ">
+        <img className="arboles__arbol uno" src={arbol1} alt="arbol"/>
+        <img className="arboles__arbol dos " src={arbol1} alt="arbol"/>
+      </div>    
     </div>
+
 
   )
 }
