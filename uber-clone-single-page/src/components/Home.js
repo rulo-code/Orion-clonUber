451aaa18395a02ad5ab915c415d4a925d675aa2c
Andres <drew.lizcano@gmail.com>
2019-09-13 17:33:12 -0500

    terminando animacion de incio mobile

--- uber-clone-single-page/src/components/Home.js
+++ uber-clone-single-page/src/components/Home.js
@@ -2,27 +2,44 @@ import React from 'react'
 
 import './styles/Home.css'
 
-import barrainclinada from './styles/images/finales/Recurso7.png'
+import barrainclinada from './styles/images/finales/barra.png'
 import ciudad from './styles/images/finales/Recurso2.png'
 import nube from './styles/images/finales/Recurso8.png'
 import arbol1 from './styles/images/finales/Recurso4.png'
-import arbol2 from './styles/images/finales/Recurso5.png'
+import carrito from './styles/images/finales/Recurso3.png'
 
 function Home(){
   return(
     <div className="home">
-      <img className="home__barra" src={barrainclinada} alt="barra"/>
-      <img className="home__ciudad" src={ciudad} alt="ciudad"/>
-      <div className="home__nuves">
-        <img className="nuves__nuve uno" src={nube} alt="nube"/>
-        <img className="nuves__nuve dos" src={nube} alt="nube"/>
-        <img className="nuves__nuve tres" src={nube} alt="nube"/>
+
+      <div className="home__hello ">
+        <h1>City</h1>
+        <p>Driver</p>
       </div>
-      <div className="home__arboles ">
-        <img className="arboles__arbol uno" src={arbol1} alt="arbol"/>
-        <img className="arboles__arbol dos " src={arbol1} alt="arbol"/>
-      </div>    
+      <div className="home__nubes">
+          <img className="nubes__nube uno" src={nube} alt="nube"/>
+          <img className="nubes__nube dos" src={nube} alt="nube"/>
+          <img className="nubes__nube tres" src={nube} alt="nube"/>
+        </div>
+      <div className="container ">
+        <img className="home__barra" src={barrainclinada} alt="barra"/>
+        <img className="home__ciudad" src={ciudad} alt="ciudad"/>
+       
+        <div className="home__arboles ">
+          <img className="arboles__arbol uno" src={arbol1} alt="arbol"/>
+          <img className="arboles__arbol dos " src={arbol1} alt="arbol"/>
+        </div>  
+        <img className="home__carrito" src={carrito} alt="carrito"/> 
+        <div className="home__form ">
+        <a className="form__btn  signIn" href="/">Sign In</a>
+        <a className="form__btn  signOut" href="/">Sign Out</a>
+      </div>
     </div>
+      </div> 
+
+
+
+      
 
 
   )
