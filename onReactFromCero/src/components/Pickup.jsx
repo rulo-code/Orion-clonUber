2754f0d0b9808f13a7b03abf2b289cebd9d2e109
Andres <drew.lizcano@gmail.com>
2019-11-14 23:03:12 -0500

    creando servicios

--- onReactFromCero/src/components/Pickup.jsx
+++ onReactFromCero/src/components/Pickup.jsx
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
+import { GoogleApiWrapper } from 'google-maps-react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
 import '../assets/styles/components/Pickup.scss';
 import FavoriteSites from './FavoriteSites';
 
 const Pickup = () => {
+  const [originValue, setOriginValue] = useState('');
+  const [origin, setOrigin] = useState({});
+  const handleChange = (originValue) => {
+    setOriginValue(originValue);
+  };
+  const handleState = (state, type) => {
+    setOrigin({
+      [type]: state,
+    });
+  };
+  const handleSelectOrigin = (originValue) => {
+    geocodeByAddress(originValue)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => handleState(latLng, 'origin'))
+      .catch((error) => console.error('Error', error));
+
+  };
   return (
     <div className='pickup'>
       <h2>¿Dónde te recogemos?</h2>
-      <input type='text' defaultValue='call 108 # 77c - 22' />
+      <PlacesAutocomplete
+        value={originValue}
+        onChange={handleChange}
+        onSelect={handleSelectOrigin}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className='autocomplete-dropdown-container'>
+              {loading && <div>Loading...</div>}
+              {suggestions.map((suggestion) => {
+                const className = suggestion.active ?
+                  'suggestion-item--active' :
+                  'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active ?
+                  { backgroundColor: '#fafafa', cursor: 'pointer' } :
+                  { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
       <FavoriteSites />
       <Link to='/dropoff' className='btn' type='button'>Siguiente</Link>
     </div>
   );
 };
-export default Pickup;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCmjvkXB_DMnBUNwxQztLMStyQmA_szbNw',
+})(Pickup);
--- onReactFromCero/src/pages/TripLayout.jsx
+++ onReactFromCero/src/pages/TripLayout.jsx
@@ -1,20 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import Header from '../components/HeaderTrip';
-import Map from '../components/Map';
+import MyMap from '../components/Map';
 import '../assets/styles/pages/TripLayout.scss';
 
-const TripLayout = ({ children }) => {
+const TripLayout = (props) => {
+  const [originValue, setOriginValue] = useState('');
+  const [destinationValue, setDestinationValue] = useState('');
+  const [origin, setOrigin] = useState({});
+  const [destination, setDestination] = useState({});
+  const [showMap, setShowMAp] = useState(false);
+
+  const handleChange = (originValue) => {
+    if (originValue.length === 0) {
+      setOrigin({});
+    }
+    setOriginValue(originValue);
+  };
+  const handleChange2 = (destinationValue) => {
+    if (destinationValue.length === 0) {
+      setDestination({});
+    }
+    setDestinationValue(destinationValue);
+  };
+  const handleOrigin = (state, type) => {
+    setOrigin(state);
+  };
+  const handleDestination = (state, type) => {
+    setDestination(state);
+  };
+  const handleSelectOrigin = (originValue) => {
+    setOriginValue(originValue);
+    console.log(`origin value: ${originValue}`);
+    geocodeByAddress(originValue)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => handleOrigin(latLng))
+      .catch((error) => console.error('Error', error));
+
+  };
+  const handleSelectDestination = (destinationValue) => {
+    setDestinationValue(destinationValue);
+    console.log(`destination value: ${destinationValue}`);
+
+    geocodeByAddress(destinationValue)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => handleDestination(latLng))
+      .catch((error) => console.error('Error', error));
+
+  };
   return (
     <div className='trip'>
-
-      <div className='trip__map'>
-        <Map />
-      </div>
-      <div className='trip__info'>
-        { children }
-      </div>
       <div className='trip__header'>
         <Header />
+      </div>
+
+      <div className='trip__info'>
+        <div className='Dashboard-header'>
+          <h2>Create a new service</h2>
+
+        </div>
+
+        <PlacesAutocomplete
+          value={originValue}
+          onChange={handleChange}
+          onSelect={handleSelectOrigin}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Origin',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className='autocomplete-dropdown-container'>
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion) => {
+                  const className = 'suggestion-item';
+                  const style = { cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+        <PlacesAutocomplete
+          value={destinationValue}
+          onChange={handleChange2}
+          onSelect={handleSelectDestination}
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Destination',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className='autocomplete-dropdown-container'>
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion) => {
+                  const className = 'suggestion-item';
+                  const style = { cursor: 'pointer' };
+                  return (
+                    <div
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+
+      </div>
+      <div className='trip__map'>
+        {origin.lat && destination.lat && (
+          <MyMap
+            origin={origin}
+            destination={destination}
+            originValue={originValue}
+            destinationValue={destinationValue}
+          />
+        )}
       </div>
     </div>
   );
