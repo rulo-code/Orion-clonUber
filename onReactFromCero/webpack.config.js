6f0b20608dc10e15d0fd24f5a2bf76bdff2c179c
Jeff Barragan <leshz@me.com>
2019-11-23 07:11:06 -0500

    change publich path

--- onReactFromCero/webpack.config.js
+++ onReactFromCero/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
   resolve: {
@@ -52,7 +52,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './src/index.html',
       filename: './index.html',
     }),
     new MiniCssExtractPlugin({
