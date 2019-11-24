bbeb46baeebd30e588e4881fd0894539d14b18b7
Jeff Barragan <leshz@me.com>
2019-11-23 20:26:30 -0500

    cambio de entry

--- onReactFromCero/src/services/entrypoints.js
+++ onReactFromCero/src/services/entrypoints.js
@@ -1,4 +1,4 @@
-// const entryPointLogin = 'https://pure-peak-47666.herokuapp.com/api/';
-const entryPointLogin = 'http://localhost:3000/api/';
+const entryPointLogin = 'https://pure-peak-47666.herokuapp.com/api/';
+// const entryPointLogin = 'http://localhost:3000/api/';
 
 export default entryPointLogin;
