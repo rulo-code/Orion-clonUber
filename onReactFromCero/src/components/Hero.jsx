9bbf3ac92dceb4c5a701465c7cbf3adbbaadf253
Andres <drew.lizcano@gmail.com>
2019-11-27 15:16:35 -0500

    media querys trip

--- onReactFromCero/src/components/Hero.jsx
+++ onReactFromCero/src/components/Hero.jsx
@@ -5,7 +5,7 @@ import Login from './Login';
 import Register from './Register';
 import Launcher from './Launcher';
 import barrainclinada from '../assets/static/barra.png';
-import ciudad from '../assets/static/Recurso2.png';
+import ciudad from '../assets/static/background.png';
 import nube from '../assets/static/Recurso8.png';
 import arbol1 from '../assets/static/Recurso4.png';
 import carrito from '../assets/static/carrito3.png';
@@ -13,21 +13,16 @@ import carrito from '../assets/static/carrito3.png';
 const Hero = ({ children }) => {
   return (
     <div className='hero'>
-      <img className='hero__barra' src={barrainclinada} alt='barra' />
       <img className='hero__ciudad' src={ciudad} alt='barra' />
+      
+      <div className='hero__slogan'>
       <img className='hero__carro' src={carrito} alt='barra' />
-
-      <div className='hero__arboles'>
-        <img className='arbol arbol1' src={arbol1} alt='barra' />
-        <img className='arbol arbol2' src={arbol1} alt='barra' />
+        <h2>Viaja Rapido y Seguro</h2>
       </div>
-
       <div className='hero__form'>
         { children }
       </div>
-      <div className='hero__slogan'>
-        <h2>Viaja Rapido y Seguro</h2>
-      </div>
+      
 
     </div>
   );
